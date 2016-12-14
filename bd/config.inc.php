1e6a96f6b0af02bc283f3f48fbb206acf38dc858
Luis Gordett <a33685@alunos.isel.pt>
2016-12-14 10:34:08 +0000

    Update config.inc.php

--- bd/config.inc.php
+++ bd/config.inc.php
@@ -1,13 +1,14 @@
 <?php
-/*$NOME_BD = "heroku_c7ebb2ee0c178dc";
 $USER_BD = "b4329ba073aa69";
 $PASS_BD = "c4688326";
-$SERVER_NAME = "eu-cdbr-west-01.cleardb.com";*/
-
+$SERVER_NAME = "eu-cdbr-west-01.cleardb.com";
+$NOME_BD = "heroku_c7ebb2ee0c178dc";
+/*
 $NOME_BD = "mesas";
 $USER_BD = "root";
 $PASS_BD = "";
 $SERVER_NAME = "localhost";
+*/
 ?>
 
 
